5d36881649fab7be6f727fcd51f934d4382697b5
DaeWoong Kim <0206woong@naver.com>
2023-06-29 22:55:14 +0900

    20230629(JavaScript)

---
+++ colors.js
@@ -0,0 +1,36 @@
+var Links = {
+    setColor : function (color){
+        var alist = document.querySelectorAll('a');
+        var i = 0;
+        while(i < alist.length){
+            alist[i].style.color = color;
+            i = i + 1;
+        }
+    }
+}
+
+var Body = {
+    setColor : function (color){
+        document.querySelector('body').style.color = color;
+    },
+    setBackGroundColor : function (color){
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
+
+function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+        Body.setBackGroundColor('black');
+        Body.setColor('white');
+        self.value = 'day'
+
+        Links.setColor('yellow');
+    } else {
+        Body.setBackGroundColor('white');
+        Body.setColor('black');
+        self.value = 'night'
+
+        Links.setColor('blue');
+    }
+}
